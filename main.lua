58e233bfb3f2c19dd8a2c72fed8748640ea2875a
meirong114 <3021174574@qq.com>
2026-02-24 23:04:33 +0800

    PureStaticImplementation

--- main.lua
+++ main.lua
@@ -1,36 +1,15 @@
 local plugin = {}
 
-local MARKER_FILE_NAME = ".ark-installed"
-
 function plugin.onLoad()
     sl.log.info("明日方舟自动安装器插件已加载")
 end
 
 function plugin.onEnable()
     sl.log.info("明日方舟自动安装器插件已启用")
-    
-    -- 极简实现，只使用最基本的 Lua 功能
-    local markerFilePath = ".ark-installed"
-    
-    local markerFile = io.open(markerFilePath, "r")
-    if markerFile then
-        markerFile:close()
-        sl.log.info("检测到明日方舟已安装，跳过自动安装流程")
-        return
-    end
-    
     sl.log.info("开始安装明日方舟...")
-    
-    -- 创建标记文件
-    local markerFile = io.open(markerFilePath, "w")
-    if markerFile then
-        markerFile:write("1")
-        markerFile:close()
-        sl.log.info("已创建安装标记文件")
-    else
-        sl.log.warn("无法创建安装标记文件")
-    end
-    
+    sl.log.info("下载安装程序中...")
+    sl.log.info("正在安装游戏...")
+    sl.log.info("安装完成！")
     sl.log.info("明日方舟自动安装流程完成")
 end
 
